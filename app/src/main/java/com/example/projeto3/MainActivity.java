2a96f4286b78529373b7d2b69dae4eae3cd5a06e
warreir <warrier@gmail.com>
2021-03-11 20:33:39 -0300

    Initial commit

---
+++ app/src/main/java/com/example/projeto3/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.projeto3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends DebugActivity {
+    private static final String CATEGORIA = "COMP_MOVEL";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(onClickLogin());
+    }
+    private View.OnClickListener onClickLogin() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tLogin = (TextView) findViewById(R.id.tLogin);
+                TextView tSenha = (TextView) findViewById(R.id.tSenha);
+                String login = tLogin.getText().toString();
+                String senha = tSenha.getText().toString();
+                if ("petri".equals(login) && "123456".equals(senha)) {
+                    // Navega para a próxima tela
+                    Intent intent = new Intent(MainActivity.this, BemVindoActivity.class);
+                    Bundle params = new Bundle();
+                    params.putString("nome", "Marcelo Petri");
+                    intent.putExtras(params);
+                    startActivity(intent);
+                } else {
+                    alert("Login e senha incorretos.");
+                }
+            }
+        };
+    }
+    private void alert(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+}
